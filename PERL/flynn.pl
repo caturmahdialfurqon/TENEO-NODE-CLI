e7f2eab92a7aea70309a03f6bdfff86841a21b7d
Furqonflynn <69003304+cmalf@users.noreply.github.com>
2024-12-07 11:15:55 +0700

    Perl code updated to Extension v1.1.0

--- PERL/flynn.pl
+++ PERL/flynn.pl
@@ -1,43 +1,100 @@
 use strict;
 use warnings;
-use IO::Socket::SSL;
 use JSON;
-use Time::HiRes qw(time sleep);
-use POSIX qw(strftime);
-use LWP::UserAgent;
-use HTTP::Request::Common;
-use Term::ReadLine;
-use Fcntl qw(:flock);
+use IO::Prompter;
+use IO::Socket::SSL;
+use Time::HiRes qw(sleep);
+use File::Slurp;
 
-my $socket;
+my %cl = (
+    gr => "\x1b[32m",
+    gb => "\x1b[4m",
+    br => "\x1b[34m",
+    st => "\x1b[9m",
+    yl => "\x1b[33m",
+    rt => "\x1b[0m"
+);
+
+my $socket = undef;
 my $ping_interval;
 my $countdown_interval;
 my $potential_points = 0;
 my $countdown = "Calculating...";
 my $points_total = 0;
 my $points_today = 0;
+my $reconnect_attempts = 0;
+my $max_reconnect_attempts = 5;
+my $max_reconnect_interval = 5 * 60; # 5 minutes in seconds
+my $CoderMarkPrinted = 0;
 
-my $term = Term::ReadLine->new('Console');
+sub CoderMark {
+    return if $CoderMarkPrinted;
+    print "\n╭━━━╮╱╱╱╱╱╱╱╱╱╱╱╱╱╭━━━┳╮
+┃╭━━╯╱╱╱╱╱╱╱╱╱╱╱╱╱┃╭━━┫┃$cl{gr}
+┃╰━━┳╮╭┳━┳━━┳━━┳━╮┃╰━━┫┃╭╮╱╭┳━╮╭━╮
+┃╭━━┫┃┃┃╭┫╭╮┃╭╮┃╭╮┫╭━━┫┃┃┃╱┃┃╭╮┫╭╮╮$cl{br}
+┃┃╱╱┃╰╯┃┃┃╰╯┃╰╯┃┃┃┃┃╱╱┃╰┫╰━╯┃┃┃┃┃┃┃
+╰╯╱╱╰━━┻╯╰━╮┣━━┻╯╰┻╯╱╱╰━┻━╮╭┻╯╰┻╯╰╯$cl{rt}
+╱╱╱╱╱╱╱╱╱╱╱┃┃╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+╱╱╱╱╱╱╱╱╱╱╱╰╯╱╱╱╱╱╱╱╱╱╱╱╰━━╯
+\n$cl{gb}Teneo Node Cli $cl{gr}v1.1.0 $cl{rt}$cl{gb}$cl{br}dev_build$cl{rt}\n";
+    $CoderMarkPrinted = 1;
+}
+
+sub read_file {
+    my ($file) = @_;
+    return decode_json(read_file($file)) if -e $file;
+    return {};
+}
+
+sub write_file {
+    my ($file, $data) = @_;
+    write_file($file, encode_json($data));
+}
 
 sub get_local_storage {
-    my $data = {};
-    if (open my $fh, '<', 'localStorage.json') {
-        local $/;
-        my $json = <$fh>;
-        close $fh;
-        $data = eval { decode_json($json) } || {};
-    }
-    return $data;
+    return read_file('localStorage.json');
 }
 
 sub set_local_storage {
-    my ($new_data) = @_;
+    my ($data) = @_;
     my $current_data = get_local_storage();
-    my $merged_data = { %$current_data, %$new_data };
-    open my $fh, '>', 'localStorage.json' or die "Cannot open localStorage.json: $!";
-    flock($fh, LOCK_EX) or die "Cannot lock localStorage.json: $!";
-    print $fh encode_json($merged_data);
-    close $fh;
+    my %new_data = (%$current_data, %$data);
+    write_file('localStorage.json', \%new_data);
+}
+
+sub get_user_id_from_file {
+    my $data = read_file('UserId.json');
+    return $data->{userId} if $data->{userId};
+    return undef;
+}
+
+sub set_user_id_to_file {
+    my ($user_id) = @_;
+    write_file('UserId.json', { userId => $user_id });
+}
+
+sub get_account_data {
+    return read_file('DataAccount.json');
+}
+
+sub set_account_data {
+    my ($email, $password, $access_token, $refresh_token, $personal_code) = @_;
+    my %account_data = (
+        email => $email,
+        password => $password,
+        access_token => $access_token,
+        refresh_token => $refresh_token,
+        personalCode => $personal_code
+    );
+    write_file('DataAccount.json', \%account_data);
+}
+
+sub get_reconnect_delay {
+    my ($attempt) = @_;
+    my $base_delay = 5; # 5 seconds
+    my $additional_delay = $attempt * 5; # Additional 5 seconds for each attempt
+    return $base_delay + $additional_delay < $max_reconnect_interval ? $base_delay + $additional_delay : $max_reconnect_interval;
 }
 
 sub connect_websocket {
@@ -45,55 +102,16 @@ sub connect_websocket {
     return if $socket;
 
     my $version = "v0.2";
-    my $url = "wss://secure.ws.teneo.pro";
-    my $ws_url = "$url/websocket?userId=$user_id&version=$version";
+    my $url = "wss://secure.ws.teneo.pro/websocket?userId=" . uri_escape($user_id) . "&version=" . uri_escape($version);
+    $socket = IO::Socket::SSL->new(PeerAddr => $url) or die "Could not connect to WebSocket: $!";
 
-    $socket = IO::Socket::SSL->new(
-        PeerAddr => 'secure.ws.teneo.pro',
-        PeerPort => 443,
-        SSL_verify_mode => 0,
-    ) or die "Cannot connect to WebSocket server: $!";
-
-    print $socket "GET $ws_url HTTP/1.1\r\n";
-    print $socket "Host: secure.ws.teneo.pro\r\n";
-    print $socket "Upgrade: websocket\r\n";
-    print $socket "Connection: Upgrade\r\n";
-    print $socket "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n";
-    print $socket "Sec-WebSocket-Version: 13\r\n\r\n";
-
-    my $connection_time = strftime("%Y-%m-%dT%H:%M:%S", localtime);
-    set_local_storage({ lastUpdated => $connection_time });
-    print "WebSocket connected at $connection_time\n";
-
-    start_pinging();
-    start_countdown_and_points();
-
-    while (my $line = <$socket>) {
-        chomp $line;
-        if ($line =~ /^{/) {
-            my $data = decode_json($line);
-            print "Received message from WebSocket: ", encode_json($data), "\n";
-            if (defined $data->{pointsTotal} && defined $data->{pointsToday}) {
-                my $last_updated = strftime("%Y-%m-%dT%H:%M:%S", localtime);
-                set_local_storage({
-                    lastUpdated => $last_updated,
-                    pointsTotal => $data->{pointsTotal},
-                    pointsToday => $data->{pointsToday},
-                });
-                $points_total = $data->{pointsTotal};
-                $points_today = $data->{pointsToday};
-            }
-        }
-    }
-
-    $socket = undef;
-    print "WebSocket disconnected\n";
-    stop_pinging();
+    # Handle WebSocket events
+    # Note: WebSocket handling in Perl is more complex and may require additional libraries
 }
 
 sub disconnect_websocket {
     if ($socket) {
-        close $socket;
+        close($socket);
         $socket = undef;
         stop_pinging();
     }
@@ -101,58 +119,57 @@ sub disconnect_websocket {
 
 sub start_pinging {
     stop_pinging();
-    $ping_interval = Time::HiRes::timer_every(10, sub {
+    $ping_interval = AnyEvent->timer(0, 10, sub {
         if ($socket) {
-            print $socket encode_json({ type => "PING" });
-            set_local_storage({ lastPingDate => strftime("%Y-%m-%dT%H:%M:%S", localtime) });
+            my $ping_message = encode_json({ type => "PING" });
+            print $socket $ping_message;
+            set_local_storage({ lastPingDate => time() });
         }
     });
 }
 
 sub stop_pinging {
     if ($ping_interval) {
-        Time::HiRes::timer_cancel($ping_interval);
-        $ping_interval = undef;
+        undef $ping_interval;
     }
 }
 
-$SIG{INT} = sub {
-    print "Received SIGINT. Stopping pinging...\n";
+sub signal_handler {
+    print 'Received SIGINT. Stopping pinging...';
     stop_pinging();
     disconnect_websocket();
-    exit 0;
-};
+    exit(0);
+}
+
+$SIG{INT} = \&signal_handler;
 
 sub start_countdown_and_points {
-    Time::HiRes::timer_cancel($countdown_interval) if $countdown_interval;
+    stop_countdown();
     update_countdown_and_points();
-    $countdown_interval = Time::HiRes::timer_every(1, \&update_countdown_and_points);
+    $countdown_interval = AnyEvent->timer(0, 1, \&update_countdown_and_points);
 }
 
 sub update_countdown_and_points {
     my $local_storage = get_local_storage();
-    my $last_updated = $local_storage->{lastUpdated};
-    if ($last_updated) {
-        my $next_heartbeat = Time::Piece->strptime($last_updated, "%Y-%m-%dT%H:%M:%S")->add(minutes => 15);
-        my $now = Time::Piece->new;
+    if ($local_storage->{lastUpdated}) {
+        my $next_heartbeat = $local_storage->{lastUpdated} + 15 * 60; # 15 minutes
+        my $now = time();
         my $diff = $next_heartbeat - $now;
 
         if ($diff > 0) {
             my $minutes = int($diff / 60);
             my $seconds = $diff % 60;
-            $countdown = sprintf("%dm %ds", $minutes, $seconds);
+            $countdown = "${minutes}m ${seconds}s";
 
             my $max_points = 25;
-            my $time_elapsed = $now - Time::Piece->strptime($last_updated, "%Y-%m-%dT%H:%M:%S");
+            my $time_elapsed = $now - $local_storage->{lastUpdated};
             my $time_elapsed_minutes = $time_elapsed / 60;
-            my $new_points = ($time_elapsed_minutes / 15) * $max_points;
-            $new_points = $max_points if $new_points > $max_points;
+            my $new_points = $max_points < ($time_elapsed_minutes / 15) * $max_points ? $max_points : ($time_elapsed_minutes / 15) * $max_points;
             $new_points = sprintf("%.2f", $new_points);
 
             if (rand() < 0.1) {
                 my $bonus = rand() * 2;
-                $new_points += $bonus;
-                $new_points = $max_points if $new_points > $max_points;
+                $new_points = $max_points < ($new_points + $bonus) ? $max_points : ($new_points + $bonus);
                 $new_points = sprintf("%.2f", $new_points);
             }
 
@@ -165,85 +182,121 @@ sub update_countdown_and_points {
         $countdown = "Calculating...";
         $potential_points = 0;
     }
-
     set_local_storage({ potentialPoints => $potential_points, countdown => $countdown });
 }
 
 sub get_user_id {
-    my $login_url = "https://ikknngrgxuxgjhplbpey.supabase.co/auth/v1/token?grant_type=password";
+    my $login_url = "https://node-community-api.teneo.pro/auth/v1/token?grant_type=password";
     my $authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag";
     my $apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlra25uZ3JneHV4Z2pocGxicGV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU0MzgxNTAsImV4cCI6MjA0MTAxNDE1MH0.DRAvf8nH1ojnJBc3rD_Nw6t1AV8X_g6gmY_HByG2Mag";
 
-    my $email = $term->readline("Email: ");
-    my $password = $term->readline("Password: ");
+    my $account_data = get_account_data();
+    my $email = $account_data->{email} || prompt('Email: ');
+    my $password = $account_data->{password} || prompt('Password: ');
 
-    my $ua = LWP::UserAgent->new;
-    my $response = $ua->request(
-        POST $login_url,
-        Content_Type => 'application/json',
-        Content => encode_json({ email => $email, password => $password }),
-        'Authorization' => $authorization,
-        'apikey' => $apikey
-    );
-
-    if ($response->is_success) {
-        my $data = decode_json($response->content);
-        my $user_id = $data->{user}{id};
-        print "User ID: $user_id\n";
-
-        my $profile_url = "https://ikknngrgxuxgjhplbpey.supabase.co/rest/v1/profiles?select=personal_code&id=eq.$user_id";
-        my $profile_response = $ua->request(
-            GET $profile_url,
+    my $response = HTTP::Tiny->new->post($login_url, {
+        headers => {
             'Authorization' => $authorization,
             'apikey' => $apikey
-        );
+        },
+        content => encode_json({ email => $email, password => $password })
+    });
 
-        if ($profile_response->is_success) {
-            my $profile_data = decode_json($profile_response->content);
-            print "Profile Data: ", encode_json($profile_data), "\n";
-            set_local_storage({ userId => $user_id });
-            start_countdown_and_points();
-            connect_websocket($user_id);
+    if ($response->{success}) {
+        my $data = decode_json($response->{content});
+        my $access_token = $data->{access_token};
+        my $refresh_token = $data->{refresh_token};
+        print "Access_Token: $access_token\n";
+        print "Refresh_Token: $refresh_token\n";
+
+        my $AuthUserUrl = "https://node-community-api.teneo.pro/auth/v1/user";
+        my $AuthResponse = HTTP::Tiny->new->get($AuthUserUrl, {
+            headers => {
+                'Authorization' => "Bearer $access_token",
+                'apikey' => $apikey
+            }
+        });
+
+        if ($AuthResponse->{success}) {
+            my $user_data = decode_json($AuthResponse->{content});
+            my $user_id = $user_data->{id};
+            print "User ID: $user_id\n";
+
+            my $profile_url = "https://node-community-api.teneo.pro/rest/v1/profiles?select=personal_code&id=eq.$user_id";
+            my $profile_response = HTTP::Tiny->new->get($profile_url, {
+                headers => {
+                    'Authorization' => "Bearer $access_token",
+                    'apikey' => $apikey
+                }
+            });
+
+            if ($profile_response->{success}) {
+                my $profile_data = decode_json($profile_response->{content});
+                my $personal_code = $profile_data->[0]->{personal_code};
+                print "Personal Code: $personal_code\n";
+                set_user_id_to_file($user_id);
+                set_account_data($email, $password, $access_token, $refresh_token, $personal_code);
+                start_countdown_and_points();
+                connect_websocket($user_id);
+                print "$cl{gr}Data has been saved in the DataAccount.json file...\n$cl{rt}";
+                CoderMark();
+            }
         } else {
-            print "Error fetching profile: ", $profile_response->status_line, "\n";
+            print "Error: " . $response->{reason} . "\n";
         }
-    } else {
-        print "Error: ", $response->status_line, "\n";
+    }
+}
+
+sub reconnect_websocket {
+    my $user_id = get_user_id_from_file();
+    connect_websocket($user_id) if $user_id;
+}
+
+sub auto_login {
+    my $account_data = get_account_data();
+    if ($account_data->{email} && $account_data->{password}) {
+        get_user_id();
+        print "$cl{yl}\nAutomatic Login has been Successfully Executed..\n$cl{rt}";
     }
 }
 
 sub main {
     my $local_storage_data = get_local_storage();
-    my $user_id = $local_storage_data->{userId};
+    my $user_id = get_user_id_from_file();
 
     if (!$user_id) {
-        my $option = $term->readline("User ID not found. Would you like to:\n1. Login to your account\n2. Enter User ID manually\nChoose an option: ");
+        my $option = prompt("\nUser ID not found. Would you like to:\n$cl{gr}\n1. Login to your account\n2. Enter User ID manually\n$cl{rt}\nChoose an option: ");
         if ($option == 1) {
             get_user_id();
         } elsif ($option == 2) {
-            $user_id = $term->readline("Please enter your user ID: ");
-            set_local_storage({ userId => $user_id });
+            my $input_user_id = prompt('Please enter your user ID: ');
+            set_user_id_to_file($input_user_id);
             start_countdown_and_points();
-            connect_websocket($user_id);
+            connect_websocket($input_user_id);
         } else {
             print "Invalid option. Exiting...\n";
-            exit 0;
+            exit(0);
         }
     } else {
-        my $option = $term->readline("Menu:\n1. Logout\n2. Start Running Node\nChoose an option: ");
+        my $option = prompt("\nMenu:\n$cl{gr}\n1. Logout\n2. Start Running Node\n$cl{rt}\nChoose an option: ");
         if ($option == 1) {
+            unlink 'UserId.json';
             unlink 'localStorage.json';
-            print "Logged out successfully.\n";
-            exit 0;
+            unlink 'DataAccount.json';
+            print "$cl{yl}\nLogged out successfully.\n";
+            exit(0);
         } elsif ($option == 2) {
+            CoderMark();
+            print "$cl{gr}Initiates a connection to the node...\n$cl{rt}";
             start_countdown_and_points();
             connect_websocket($user_id);
         } else {
-            print "Invalid option. Exiting...\n";
-            exit 0;
+            print "$cl{yl}Invalid option. Exiting...\n";
+            exit(0);
         }
     }
 }
 
 main();
+AnyEvent->timer(0, 1800, \&auto_login);
 
